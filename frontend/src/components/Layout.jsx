1aeb4e6c18214660732be90e9f1cde4557e0f68a
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-10-16 23:57:33 -0700

    added routing to navbar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,29 +2,48 @@ import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function Layout() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-  <div className="bg-gray-800 h-screen text-white">
-    <div className='grid grid-cols-5 gap-4 bg-gray-800 h-[100px] border-b-2 border-gray-600'>
-      
-      <button className='bg-gray-800 rounded-lg m-2 hover:bg-gray-900'>b</button>
-      <button className='bg-gray-800 rounded-lg m-2 hover:bg-gray-900'>b</button>
-      <button className='bg-gray-800 rounded-lg m-2 hover:bg-gray-900'>b</button>
-      <button className='bg-gray-800 rounded-lg m-2 hover:bg-gray-900'>b</button>
-      <button className='bg-gray-800 rounded-lg m-2 hover:bg-gray-900'>b</button>
+    return (
+        <div className="bg-gray-800 h-screen text-white">
+            <div className='grid grid-cols-5 gap-4 bg-gray-800 h-[100px] border-b-2 border-gray-600'>
 
-    </div>
+                <Link
+                    to="/"
+                    className='bg-gray-900 rounded-lg m-2 hover:bg-gray-950 text-center content-center'
+                >Home
+                </Link>
+                <Link
+                    to="/hiragana"
+                    className='bg-gray-900 rounded-lg m-2 hover:bg-gray-950 text-center content-center'
+                >hiragana
+                </Link>
+                <Link
+                    to="/hiraganaWords"
+                    className='bg-gray-900 rounded-lg m-2 hover:bg-gray-950 text-center content-center'
+                >hiragana words
+                </Link>
+                <Link
+                    to="/katakana"
+                    className='bg-gray-900 rounded-lg m-2 hover:bg-gray-950 text-center content-center'
+                >katakana
+                </Link>
+                <Link
+                    to="/katakanaWords"
+                    className='bg-gray-900 rounded-lg m-2 hover:bg-gray-950 text-center content-center'
+                >katakana words
+                </Link>
 
-    <div className='h-2/3 p-10 content-center '>
+            </div>
 
-        <Outlet />
+            <div className='h-2/3 p-10 content-center '>
 
-    </div>
+                <Outlet />
 
-    </div>
-  )
+            </div>
+
+        </div>
+    )
 }
 
 export default Layout;
- 
