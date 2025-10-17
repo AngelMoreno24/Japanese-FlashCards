3991dd23f0d1d2f1b12413e48107f8e3dcf4cda2
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-10-17 00:07:35 -0700

    added routing to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Outlet } from "react-router-dom";
 
 const Home = () => { 
   
@@ -6,35 +7,47 @@ const Home = () => {
   
       <div className=' p-10 content-center grid grid-cols-2 grid-rows-3 gap-7'>
         
-        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 hover:bg-gray-950 hover:border-gray-700 '>
-            
+        <Link
+            to="/hiragana"
+            className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 hover:bg-gray-950 hover:border-gray-700 '
+        >            
             <h1 className=" text-[30px] font-bold ">
             Hiragana Alphabet
             </h1> 
             <p className=''>practice hiragana characters with flashcards</p>
-        </div>
+        </Link>
 
-        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10'>
-            
+        <Link
+            to="/hiraganawords"
+            className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 hover:bg-gray-950 hover:border-gray-700 '
+        >            
+
             <h1 className=" text-[30px] font-bold ">
             Hiragana Words
             </h1> 
             <p className=''>practice hiragana words with flashcards</p>
-        </div>
-        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10'>
-            
+        </Link>
+
+        <Link
+            to="/katakana"
+            className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 hover:bg-gray-950 hover:border-gray-700 '
+        >            
+
             <h1 className=" text-[30px] font-bold ">
             Katakana Alphabet
             </h1> 
-            <p className=''>practice hiragana characters with flashcards</p>
-        </div>
-        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 '>
-            
+            <p className=''>practice Katakana characters with flashcards</p>
+        </Link>
+        <Link
+            to="/katakanawords"
+            className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 hover:bg-gray-950 hover:border-gray-700 '
+        >            
+
             <h1 className=" text-[30px] font-bold ">
             Katakana Words
             </h1> 
-            <p className=''>practice hiragana characters with flashcards</p>
-        </div>
+            <p className=''>practice katakana words with flashcards</p>
+        </Link>
 
       </div>
   
