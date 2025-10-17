6e915472969ecaaed3c04366f3b2f836802e325f
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-10-16 23:25:10 -0700

    added cards for home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,12 +4,38 @@ const Home = () => {
   
     return (
   
-      <div className='h-[220px] p-10 content-center '>
-  
-        <h1 className="text-[50px] font-bold underline text-center">
-          Hello world!
-        </h1>
-  
+      <div className=' p-10 content-center grid grid-cols-2 grid-rows-3 gap-7'>
+        
+        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 hover:bg-gray-950 hover:border-gray-700 '>
+            
+            <h1 className=" text-[30px] font-bold ">
+            Hiragana Alphabet
+            </h1> 
+            <p className=''>practice hiragana characters with flashcards</p>
+        </div>
+
+        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10'>
+            
+            <h1 className=" text-[30px] font-bold ">
+            Hiragana Words
+            </h1> 
+            <p className=''>practice hiragana words with flashcards</p>
+        </div>
+        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10'>
+            
+            <h1 className=" text-[30px] font-bold ">
+            Katakana Alphabet
+            </h1> 
+            <p className=''>practice hiragana characters with flashcards</p>
+        </div>
+        <div className='bg-gray-900  text-center border-1 rounded-lg border-gray-600 w-full h-auto p-10 '>
+            
+            <h1 className=" text-[30px] font-bold ">
+            Katakana Words
+            </h1> 
+            <p className=''>practice hiragana characters with flashcards</p>
+        </div>
+
       </div>
   
   )
